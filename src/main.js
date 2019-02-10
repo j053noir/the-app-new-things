e8cae38d6c256daf40045938b5c56c3468719151
Jose Alejandro Rojano Crisson <j053noir@gmail.com>
2019-02-10 13:12:43 -0500

    Added edit and trash icons from fontawesome

--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.config.productionTip = false;
+import App from './App.vue';
+
+import router from './router';
+import store from './store';
+
+library.add(faEdit);
+library.add(faTrash);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
